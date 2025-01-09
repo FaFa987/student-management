07098256d8ebb06bb622f40a65bfe52ce0ad8371
Fatemeh <f.khalleghi.777@gmail.com>
2025-01-09 13:01:19 +0330

    Part 1-3

--- src/main/java/se/lexicon/data_access/StudentDaoListImpl.java
+++ src/main/java/se/lexicon/data_access/StudentDaoListImpl.java
@@ -1,6 +1,16 @@
 package se.lexicon.data_access;
 
+import se.lexicon.models.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class StudentDaoListImpl implements StudentDao{
 
+    private List<Student> students;
+
+    public StudentDaoListImpl() {
+        this.students = new ArrayList<Student>();
+    }
 }
