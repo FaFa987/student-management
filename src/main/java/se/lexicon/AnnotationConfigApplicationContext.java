080cf5ff56caf5eeccb0da9b420fd65a7f7c6b72
Fatemeh <f.khalleghi.777@gmail.com>
2025-01-09 12:53:46 +0330

    Part 1-2

--- src/main/java/se/lexicon/AnnotationConfigApplicationContext.java
+++ src/main/java/se/lexicon/AnnotationConfigApplicationContext.java
@@ -4,8 +4,10 @@ import se.lexicon.data_access.StudentDao;
 
 public class AnnotationConfigApplicationContext {
     public AnnotationConfigApplicationContext(Class<ConfigFileName> configFileNameClass) {
+        return;
     }
 
     public StudentDao getBean(Class<StudentDao> studentDaoClass) {
+        return null;
     }
 }
