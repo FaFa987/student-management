ece82f8c8d530e314f90b92a8ea86654f603e68e
Fatemeh <f.khalleghi.777@gmail.com>
2025-01-09 12:53:46 +0330

    Part 1-2

--- src/main/java/se/lexicon/models/Student.java
+++ src/main/java/se/lexicon/models/Student.java
@@ -1,4 +1,37 @@
 package se.lexicon.models;
 
+import java.util.UUID;
+
 public class Student {
+    private int id;
+    private String name;
+
+    public Student(int id, String name) {
+        this.id = Integer.parseInt(UUID.randomUUID().toString());
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
